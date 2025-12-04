587de3c742761de5008d39f64a7e995c1d566d2b
김민지 <minji@air.local>
2025-12-04 21:54:43 +0900

    책 정보 가져오기

--- lib/services/book_service.dart
+++ lib/services/book_service.dart
@@ -6,7 +6,7 @@ import 'package:booging2/models/book.dart';
 
 class BookService {
 
-  // (기존) 책 검색 함수
+  //책 검색 함수
   Future<List<Book>> fetchBooks({String query = 'best seller'}) async {
     final String apiUrl = "https://www.googleapis.com/books/v1/volumes?q=$query";
 
@@ -21,7 +21,7 @@ class BookService {
     }
   }
 
-  // ⬇️ [추가된 부분] 책 ID로 상세 정보 가져오는 함수
+  // 책 ID로 상세 정보 가져오는 함수
   Future<Book?> fetchBookDetails(String bookId) async {
     // Google Books API는 ID로 책을 조회하는 주소를 따로 제공합니다.
     final String apiUrl = "https://www.googleapis.com/books/v1/volumes/$bookId";
