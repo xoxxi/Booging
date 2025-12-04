2b17357950631b0194239b087740cd0b5d5aaf5c
김민지 <minji@air.local>
2025-12-04 22:09:44 +0900

    그룹 클래스

--- lib/models/group.dart
+++ lib/models/group.dart
@@ -6,7 +6,7 @@ class Group {
   final String groupName;
   final String currentBookTitle;
   final String currentBookCoverUrl;
-  final String currentBookId; // 1. [추가] 현재 읽는 책의 ID
+  final String currentBookId; // 현재 읽는 책의 ID
   final int memberCount;
   final bool hasNewActivity;
 
@@ -14,7 +14,7 @@ class Group {
     required this.groupName,
     required this.currentBookTitle,
     required this.currentBookCoverUrl,
-    required this.currentBookId, // 2. [추가]
+    required this.currentBookId,
     required this.memberCount,
     required this.hasNewActivity,
 
